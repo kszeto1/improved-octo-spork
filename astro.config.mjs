e27fde0bc9264d9dc80777998ec116859eb01922
kszeto1 <kenny.szeto1@gmail.com>
2024-09-10 12:09:06 -0700

    Fix build and deploy issues with config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,41 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx(),
-		icon()
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: { enabled: true }
-	}),
-	vite: {
-		ssr: {
-			noExternal: ['@iconify/tools']
-		}
-	}
-})
+  site: 'https://kszeto1.github.io',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
